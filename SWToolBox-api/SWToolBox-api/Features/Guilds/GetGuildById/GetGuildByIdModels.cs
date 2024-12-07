fd966254e151c4846e655d740a5d2a737cb56df9
Aymeric Breton <aymericbreton8535@gmail.com>
2024-12-08 00:45:18 +0100

    get guild returns guild defenses

--- SWToolBox-api/SWToolBox-api/Features/Guilds/GetGuildById/GetGuildByIdModels.cs
+++ SWToolBox-api/SWToolBox-api/Features/Guilds/GetGuildById/GetGuildByIdModels.cs
@@ -2,36 +2,59 @@
 using OneOf;
 using OneOf.Types;
 using SWToolBox_api.Database.Entities;
+using SWToolBox_api.Features.Guilds.Defenses.CreateDefense;
 
 namespace SWToolBox_api.Features.Guilds.GetGuildById;
 
 public record GetGuildByIdQuery(Guid Id) : IRequest<OneOf<Guild, NotFound>>;
-public record GetGuildByIdResponse(Guid Id, string Name, IEnumerable<PlayerResponse> Players);
-public record PlayerResponse(Guid Id, string Name, IEnumerable<DefenseResponse> Defenses);
-public record DefenseResponse(MonsterResponse MonsterLead, MonsterResponse Monster2, MonsterResponse Monster3, short Wins, short? Losses);
+public record GetGuildByIdResponse(Guid Id, string Name, IEnumerable<PlayerResponse> Players, IEnumerable<GuildDefenseResponse> Defenses);
+public record GuildDefenseResponse(DefenseResponse Defense, string Description);
+public record PlayerResponse(Guid Id, string Name, IEnumerable<PlayerDefenseResponse> Defenses);
+public record PlayerDefenseResponse(DefenseResponse Defense, short Wins, short? Losses);
+public record DefenseResponse(MonsterResponse MonsterLead, MonsterResponse Monster2, MonsterResponse Monster3);
 public record MonsterResponse(long Id, string Name);
 
 public static class GetGuildByIdMapper
 {
-
     public static GetGuildByIdResponse ToResponse(this Guild guild)
     {
-        return new GetGuildByIdResponse(guild.Id, guild.Name, guild.GuildPlayers.Select(p => p.Player.ToResponse()));
+        return new GetGuildByIdResponse(
+            guild.Id,
+            guild.Name,
+            guild.GuildPlayers.Select(gp => gp.Player.ToResponse()),
+            guild.GuildDefenses.Select(gd => gd.ToResponse())
+        );
     }
 
     private static PlayerResponse ToResponse(this Player player)
     {
-        return new PlayerResponse(player.Id, player.Name, player.PlayerDefenses.Select(d => d.ToResponse()));
+        return new PlayerResponse(
+            player.Id,
+            player.Name,
+            player.PlayerDefenses.Select(d => d.ToResponse())
+        );
     }
 
-    private static DefenseResponse ToResponse(this PlayerDefense playerDefense)
+    private static PlayerDefenseResponse ToResponse(this PlayerDefense playerDefense)
     {
-        return new DefenseResponse(
-            playerDefense.Defense.MonsterLead.ToResponse(),
-            playerDefense.Defense.Monster2.ToResponse(),
-            playerDefense.Defense.Monster3.ToResponse(),
+        return new PlayerDefenseResponse(
+            playerDefense.Defense.ToResponse(),
             playerDefense.Wins,
             playerDefense.Losses
+        );
+    }
+
+    private static GuildDefenseResponse ToResponse(this GuildDefense guildDefense)
+    {
+        return new GuildDefenseResponse(guildDefense.Defense.ToResponse(), guildDefense.Description);
+    }
+
+    private static DefenseResponse ToResponse(this Defense defense)
+    {
+        return new DefenseResponse(
+            defense.MonsterLead.ToResponse(),
+            defense.Monster2.ToResponse(),
+            defense.Monster3.ToResponse()
         );
     }
 
