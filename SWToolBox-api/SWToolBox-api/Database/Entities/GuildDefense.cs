433b815e7b20713264afa2852ce5adfa1e3c910b
Aymeric Breton <aymericbreton8535@gmail.com>
2024-12-08 00:01:29 +0100

    added table

---
+++ SWToolBox-api/SWToolBox-api/Database/Entities/GuildDefense.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SWToolBox_api.Database.Entities;
+
+public partial class GuildDefense
+{
+    public Guid GuildId { get; set; }
+
+    public Guid DefenseId { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public virtual Defense Defense { get; set; } = null!;
+
+    public virtual Guild Guild { get; set; } = null!;
+}
