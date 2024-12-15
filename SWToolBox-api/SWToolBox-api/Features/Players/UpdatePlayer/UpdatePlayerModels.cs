f2afe743209c33d8fa83714a0d2c2cce0a8a2be5
Aymeric Breton <aymericbreton8535@gmail.com>
2024-12-15 01:49:10 +0100

    update player

---
+++ SWToolBox-api/SWToolBox-api/Features/Players/UpdatePlayer/UpdatePlayerModels.cs
@@ -0,0 +1,32 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OneOf;
+using OneOf.Types;
+using SWToolBox_api.Database.Entities;
+
+namespace SWToolBox_api.Features.Players.UpdatePlayer;
+
+public record UpdatePlayerCommand([FromRoute] Guid Id, string Name) : IRequest<OneOf<Player, NotFound>>;
+public record UpdatePlayerResponse(Guid Id, string Name);
+
+public class CreatePlayerValidator : Validator<UpdatePlayerCommand>
+{
+    public CreatePlayerValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("The name of the player is required.")
+            .Must(x => x.Length is > 5 and < 50)
+            .WithMessage("The name of the player must be between 5 and 50 characters.");
+    }
+}
+
+public static class UpdatePlayerMapper
+{
+    public static UpdatePlayerResponse ToResponse(this Player player)
+    {
+        return new UpdatePlayerResponse(player.Id, player.Name);
+    }
+}
