086585ec70543bd9b0428a2fd393a1f0a118f1e1
Aymeric Breton <aymericbreton8535@gmail.com>
2024-12-15 01:42:34 +0100

    delete placement

---
+++ SWToolBox-api/SWToolBox-api/Features/Guilds/Placements/DeletePlacement/DeletePlacementEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace SWToolBox_api.Features.Guilds.Placements.DeletePlacement;
+
+[HttpDelete("{id:guid}")]
+public class DeletePlacementEndpoint(ISender sender) : Endpoint<DeletePlacementCommand, Results<Ok, NotFound>>
+{
+    public override async Task<Results<Ok, NotFound>> ExecuteAsync(DeletePlacementCommand req, CancellationToken ct)
+    {
+        var successOrNotFound = await sender.Send(req, ct);
+        
+        return successOrNotFound.Match<Results<Ok, NotFound>>(
+            success => TypedResults.Ok(),
+            notFound => TypedResults.NotFound()
+        );
+    }
+}
