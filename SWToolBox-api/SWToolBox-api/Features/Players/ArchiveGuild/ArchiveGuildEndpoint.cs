2be5cea67af77435a597829868791af56ddb6fae
Aymeric Breton <aymericbreton8535@gmail.com>
2024-12-18 12:43:55 +0100

    archiveguild feature

---
+++ SWToolBox-api/SWToolBox-api/Features/Players/ArchiveGuild/ArchiveGuildEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace SWToolBox_api.Features.Players.ArchiveGuild;
+
+[HttpPatch("{id:guid}/guilds/{guildId:guid}/archive")]
+[Group<PlayersGroup>]
+[Authorize(Policy = "ManagePlayerData")]
+public class ArchiveGuildEndpoint(ISender sender) : Endpoint<ArchiveGuildCommand, Results<Ok, NotFound>>
+{
+    public override async Task<Results<Ok, NotFound>> ExecuteAsync(ArchiveGuildCommand req, CancellationToken ct)
+    {
+        var successOrNotFound = await sender.Send(req, ct);
+
+        return successOrNotFound.Match<Results<Ok, NotFound>>(
+            success => TypedResults.Ok(),
+            notFound => TypedResults.NotFound()
+        );
+    }
+}
