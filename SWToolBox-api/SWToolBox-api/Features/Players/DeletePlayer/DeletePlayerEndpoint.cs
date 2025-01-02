146ca0cfbdcbb62bea3d8c706a02c864827d534e
Aymeric Breton <aymericbreton8535@gmail.com>
2025-01-02 23:59:47 +0100

    delete player feature

--- SWToolBox-api/SWToolBox-api/Features/Players/DeletePlayer/DeletePlayerEndpoint.cs
+++ SWToolBox-api/SWToolBox-api/Features/Players/DeletePlayer/DeletePlayerEndpoint.cs
@@ -1,6 +1,20 @@
-﻿namespace SWToolBox_api.Features.Players.DeletePlayer;
+﻿using FastEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 
-public class DeletePlayerEndpoint
+namespace SWToolBox_api.Features.Players.DeletePlayer;
+
+[HttpDelete("{id:guid}")]
+[Group<PlayersGroup>]
+public class DeletePlayerEndpoint(ISender sender) : Endpoint<DeletePlayerCommand, Results<Ok, NotFound>>
 {
-    
+    public override async Task<Results<Ok, NotFound>> ExecuteAsync(DeletePlayerCommand req, CancellationToken ct)
+    {
+        var successOrNotFound = await sender.Send(req, ct);
+        
+        return successOrNotFound.Match<Results<Ok, NotFound>>(
+            success => TypedResults.Ok(),
+            notFound => TypedResults.NotFound()
+        );
+    }
 }
