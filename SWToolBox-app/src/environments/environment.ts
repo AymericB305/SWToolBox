adec7b886307e22e4568394022dcf0644d63d3ab
Aymeric Breton <aymericbreton8535@gmail.com>
2025-02-15 00:23:51 +0100

    setup supabase auth

---
+++ SWToolBox-app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://wppuuivsgzvzsfpvrrxu.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwcHV1aXZzZ3p2enNmcHZycnh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0NDYyMjMsImV4cCI6MjA1MDAyMjIyM30.YN408Rv9xCcjiM9nH2wG0t5NYTmfeNYw6HruhtlVvTk'
+};
